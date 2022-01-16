50fd7dbdb146141a06d04718ce2fc35434dfdd8f
Luiz Paulo <lprodrigues451@gmail.com>
2022-01-16 00:10:09 -0300

    adicionar nas três imagem os numero 1,2 e 3

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const imagem = document.querySelector('#rick');
+const imagem1 = document.querySelector('#rick');
+const imagem2 = document.querySelector('#and');
 const botao = document.querySelector('button');
-const nomeDoPersonagem = document.querySelector('#nome');
+const botao2 = document.querySelector('button');
+const nomeDoPersonagem1 = document.querySelector('#nome1');
+const nomeDoPersonagem2 = document.querySelector('#nome2');
 const especie1 = document.querySelector('#especie1');
+const especie2 = document.querySelector('#especie2');
 const condicao1 = document.querySelector('#vivoOuMorto1');
+const condicao2 = document.querySelector('#vivoOuMorto2');
 
 traduzirCondicao1 = (data) => {
     if(data.status == 'unknown'){
@@ -13,6 +18,17 @@ traduzirCondicao1 = (data) => {
         return 'Não. Está morto';
     }
 }
+
+traduzirCondicao2 = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    }
+}
+
 
 gerarValorAletorio = () => {
     return Math.floor(Math.random() * 671);
@@ -27,12 +43,33 @@ pegarPersonagem = () => {
             "Content-type": 'application/json'
         }
     }).then((response) => response.json()).then((data) => {
-        imagem.src = data.image;
-        imagem.alt = data.name;
-        nomeDoPersonagem.innerHTML = data.name;
+        imagem1.src = data.image;
+        imagem1.alt = data.name;
+        nomeDoPersonagem1.innerHTML = data.name;
         especie1.innerHTML = data.species;
         condicao1.innerHTML = traduzirCondicao1(data);
+        
     });
 }
 
-botao.onclick = pegarPersonagem;
+pegarPersonagem2 = () => {
+    let numeroAleatorio = gerarValorAletorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem2.src = data.image;
+        imagem2.alt = data.name;
+        nomeDoPersonagem2.innerHTML = data.name;
+        especie2.innerHTML = data.species;
+        condicao2.innerHTML = traduzirCondicao2(data);
+        
+    });
+}
+
+botao.onclick = pegarPersonagem2;
+botao2.onclick = pegarPersonagem;
+
